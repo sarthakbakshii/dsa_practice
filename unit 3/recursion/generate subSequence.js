dabab713af8ee450926018e3d2c2fcd750ecb42a
sarthakbakshii <sarthakbakshi14@gmail.com>
2021-12-04 20:18:04 +0530

    x

--- unit 3/recursion/generate subSequence.js
+++ unit 3/recursion/generate subSequence.js
@@ -53,3 +53,21 @@ First output all the subsequences starting with first character.
  such starting with second chars, and so on.
 ------------------------------------------------------------------------------------------------------------------*/
 
+const subarrayFromI = (str,ans="",current = 0) =>{
+    if( ans.length > 0) console.log(ans);
+    if( current == str.length) return;
+    
+    for( let j = current ; j < str.length ; j++ ){
+        ans += str[j];
+        subarrayFromI(str,ans,j+1);
+        ans = ans.slice(0,-1)
+    }
+}
+
+function runProgram(input) {
+  a = input.split("\n");
+  let l = +a[0];
+  let str = a[1];
+  
+  subarrayFromI(str) 
+}
